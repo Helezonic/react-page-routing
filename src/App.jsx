5de2b142938dd7b7e86ba1b56a71a0d156592acc
Shuaib <ahamed.shuaib10@gmail.com>
2024-07-24 02:56:35 +0530

    --new project commit #1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+//import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-cyan-100 fixed h-screen w-screen">
+        <div className="relative mt-4">
+          <h1 className="w-1/4 border p-4 text-center text-3xl font-serif  mx-auto">Work</h1>
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
